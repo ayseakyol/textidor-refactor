3ece601d9cf8f40e4446ae235d65ab29627c8a15
ayseakyol <ayseakyol.22309@gmail.com>
2020-05-18 17:03:34 +0200

    async refactor

--- api/handlers.js
+++ api/handlers.js
@@ -1,12 +1,94 @@
-const fs = require('fs');
-const path = require('path');
-const config = require('../config');
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+const config = require("../config");
+
+const readDir = util.promisify(fs.readdir);
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+const deleteFile = util.promisify(fs.unlink);
 
 // define FILES_DIR
-const FILES_DIR = path.join(_);
+const FILES_DIR = path.join(__dirname, "../files-dev");
 
 // declare the handlers
-const handlers = {};
+
+const handlers = {
+  getFiles: async (req, res, next) => {
+    try {
+      const list = await readDir(FILES_DIR);
+      res.json(list);
+    } catch (err) {
+      if (err && err.code === "ENOENT") {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+        return;
+      }
+    }
+  },
+  readFile: async (req, res, next) => {
+    const fileName = req.params.name;
+    try {
+      const fileText = await readFile(`${FILES_DIR}/${fileName}`, "utf-8");
+      const responseData = {
+        name: fileName,
+        text: fileText,
+      };
+      res.json(responseData);
+    } catch (err) {
+      if (err && err.code === "ENOENT") {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+        return;
+      }
+    }
+  },
+
+  writeFile: async (req, res, next) => {
+    const fileName = req.params.name;
+    const fileText = req.body.text;
+    try {
+      await writeFile(`${FILES_DIR}/${fileName}`, fileText);
+      // refactor hint:
+      res.redirect(303, "/api/files");
+    } catch (err) {
+      if (err && err.code === "ENOENT") {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+        return;
+      }
+
+      // handlers.getFiles(req, res, next);
+    }
+  },
+  deleteFile: async (req, res, next) => {
+    const fileName = req.params.name;
+    try {
+      await deleteFile(`${FILES_DIR}/${fileName}`);
+      // refactor hint:
+      res.redirect(303, "/api/files");
+    } catch (err) {
+      if (err && err.code === "ENOENT") {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+        return;
+      }
+    }
+    // handlers.getFiles(req, res, next);
+  },
+};
 
 // export the handlers
-_;
+module.exports = handlers;
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
-const config = require('./config');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const config = require("./config");
+const api = require("./api/routes");
 
 // - setup -
 const FILES_DIR = path.join(__dirname, config.FILES_DIR);
@@ -21,96 +22,18 @@ app.use(bodyParser.json());
 
 // https://github.com/expressjs/morgan#write-logs-to-a-file
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' }
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
 );
-app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan("combined", { stream: accessLogStream }));
 // and log to the console
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // statically serve the frontend
-app.use('/', express.static(path.join(__dirname, 'client')));
+app.use("/", express.static(path.join(__dirname, "client")));
 
 // ------ refactor everything from here .....
-
-
-app.get('/api/files', (req, res, next) => {
-  fs.readdir(FILES_DIR, (err, list) => {
-    if (err && err.code === 'ENOENT') {
-      res.status(404).end();
-      return;
-    }
-    if (err) {
-      next(err);
-      return;
-    }
-
-    res.json(list);
-  });
-});
-
-// read a file
-//  called by action: fetchAndLoadFile
-app.get('/api/files/:name', (req, res, next) => {
-  const fileName = req.params.name;
-  fs.readFile(`${FILES_DIR}/${fileName}`, 'utf-8', (err, fileText) => {
-    if (err && err.code === 'ENOENT') {
-      res.status(404).end();
-      return;
-    }
-    if (err) {
-      next(err);
-      return;
-    }
-
-    const responseData = {
-      name: fileName,
-      text: fileText,
-    };
-    res.json(responseData);
-  });
-});
-
-// write a file
-//  called by action: saveFile
-app.post('/api/files/:name', (req, res, next) => {
-  const fileName = req.params.name;
-  const fileText = req.body.text;
-  fs.writeFile(`${FILES_DIR}/${fileName}`, fileText, err => {
-    if (err && err.code === 'ENOENT') {
-      res.status(404).end();
-      return;
-    }
-    if (err) {
-      next(err);
-      return;
-    }
-
-    // refactor hint:
-    res.redirect(303, '/api/files');
-    // handlers.getFiles(req, res, next);
-  });
-});
-
-// delete a file
-//  called by action: deleteFile
-app.delete('/api/files/:name', (req, res, next) => {
-  const fileName = req.params.name;
-  fs.unlink(`${FILES_DIR}/${fileName}`, err => {
-    if (err && err.code === 'ENOENT') {
-      res.status(404).end();
-      return;
-    }
-    if (err) {
-      next(err);
-      return;
-    }
-
-    // refactor hint:
-    res.redirect(303, '/api/files');
-    // handlers.getFiles(req, res, next);
-  });
-});
+app.use("/api", api);
 
 // ..... to here ------
 
@@ -126,4 +49,3 @@ app.listen(config.PORT, () => {
     `listening at http://localhost:${config.PORT} (${config.MODE} mode)`
   );
 });
-
